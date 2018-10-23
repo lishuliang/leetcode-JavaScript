e6dfb03034cc41166e8115d2f2397616a86e9b12
Li Shuliang <532784460@qq.com>
2018-10-23 21:53:31 +0800

    add 141 142

---
+++ leetcode/141.环形链表.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+let hasCycle = function(head) {
+    if(!head || !head.next)
+        return false;
+    let slow = head;
+    let fast = head.next;
+    while(slow != fast) {
+        if(fast === null || fast.next === null)
+            return false;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return true;
+};
---
+++ leetcode/142.环形链表II.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+let detectCycle = function(head) { 
+    if(!head || !head.next)
+        return null;
+    let slow = head,
+        fast = head;
+    while(fast !== null && fast.next !== null){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow === fast)
+            break;
+    }
+    if (fast === null || fast.next === null) 
+        return null;
+    slow = head;
+    while(slow !== fast){
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+};
